94dedef89d151fe62b61b9107ad4aa2e59337c60
arianeam <47660205+arianeam@users.noreply.github.com>
2022-05-18 18:21:42 -0300

    dht11 com fila
    implementada uma fila para passar os parâmetros do dht11 para outra task. Foi criada uma task teste

--- sensores/SENSORES_ESP_IDF/main/main.c
+++ sensores/SENSORES_ESP_IDF/main/main.c
@@ -1,32 +1,89 @@
 #include <stdio.h>
 
 #include "dht.h"
-//#include "sdkconfig.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/queue.h>
 
-void task_dht (void *pvParameters);
+// -------------Protótipos das tarefas (tasks)------------
+void task_dht(void *pvParameters);
+void task_teste_queue(void *pvParameters);
+//--------------------------------------------------------
 
+//------------struct e fila para passagem dos dados do DHT11 para o display-------------
+struct DHT11_param
+{
+    float umid;
+    float temp;
+
+} dht11_parametros;
+
+QueueHandle_t xQueue_dht11;
+//---------------------------------------------------------------------------------------
+
+//-----------Main------------------------------------------
 void app_main(void)
 {
-    xTaskCreate(task_dht, "task_dht", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+    xTaskCreate(task_dht, "task_dht", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
+    xTaskCreate(task_teste_queue, "task_teste_queue", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
 }
+//---------------------------------------------------------
 
+//---------Implementação das tarefas (tasks)----------------
+void task_dht(void *pvParameters)
+{
 
-void task_dht (void *pvParameters) {
-    
-    float temperatura, umidade;
+    struct DHT11_param dht11_parametros_lidos;
 
     gpio_set_pull_mode(18, GPIO_PULLUP_ONLY);
 
+    xQueue_dht11 = xQueueCreate(10, sizeof(struct DHT11_param));
+
+    if (xQueue_dht11 == 0)
+    {
+        printf("Erro ao criar fila!");
+    }
 
     while (1)
     {
-        if (dht_read_float_data(DHT_TYPE_DHT11, 18, &umidade, &temperatura) == ESP_OK)
-            printf("Umidade: %.1f%% Temp: %.1fC\n", umidade, temperatura);
+        if (dht_read_float_data(DHT_TYPE_DHT11, 18, &(dht11_parametros_lidos.umid), &(dht11_parametros_lidos.temp)) == ESP_OK)
+        {
+
+            printf("Umidade: %.1f%% Temp: %.1fC\n", dht11_parametros_lidos.umid, dht11_parametros_lidos.temp);
+
+            xQueueSend(xQueue_dht11, (void *)&dht11_parametros_lidos, (TickType_t)10);
+        }
+
         else
             printf("Erro ao ler dados do sensor dht11\n");
-        
+
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
+
+void task_teste_queue(void *pvParameters)
+{
+    struct DHT11_param dht11_receber_parametros;
+    float temperatura, umidade;
+
+    while (1)
+    {
+
+        if (xQueue_dht11 != 0)
+        {
+
+            if (xQueueReceive(xQueue_dht11, &(dht11_receber_parametros), (TickType_t)10))
+            {
+                printf("Umidade recebida: %.1f%% Temp recebida: %.1fC\n", dht11_receber_parametros.umid, dht11_receber_parametros.temp);
+            }
+            else
+            {
+
+                printf("Erro ao receber fila!");
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+//--------------------------------------------------------------------
