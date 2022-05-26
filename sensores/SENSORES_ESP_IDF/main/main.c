1e18f8dd15d0fd24a93050d1ef5e9a2213ebea1d
arianeam <47660205+arianeam@users.noreply.github.com>
2022-05-26 16:51:34 -0300

    Update main.c
    Inserida task para o adc para uso do sensor hl-69 (umidade solo).

--- sensores/SENSORES_ESP_IDF/main/main.c
+++ sensores/SENSORES_ESP_IDF/main/main.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "dht.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/queue.h>
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+#define DEFAULT_VREF 1100 // Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES 64  // Multisampling
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc_unit_t unit = ADC_UNIT_1;
+
+static esp_adc_cal_characteristics_t *adc_chars;
+
+//------Config. ESP 32----------------------------------------------------------
+static const adc_channel_t channel = ADC_CHANNEL_6; // GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
+//-----------------------------------------------------------------------------
+
+//--------------Protótipos das funções---------------------
+static void print_char_val_type(esp_adc_cal_value_t val_type);
+static void check_efuse(void);
+//---------------------------------------------------------
 
 // -------------Protótipos das tarefas (tasks)------------
 void task_dht(void *pvParameters);
 void task_teste_queue(void *pvParameters);
+void task_adc_hl_69(void *pvParameters);
 //--------------------------------------------------------
 
 //------------struct e fila para passagem dos dados do DHT11 para o display-------------
@@ -21,13 +42,38 @@ struct DHT11_param
 QueueHandle_t xQueue_dht11;
 //---------------------------------------------------------------------------------------
 
+
+
 //-----------Main------------------------------------------
 void app_main(void)
 {
+
+    //----------------------------------------
+    /*
+    Trecho disponível no exemplo "adc1_example_main.c" do fabricante Espressif
+    que pode ser encontrada no link:
+    https://github.com/espressif/esp-idf/tree/master/examples/peripherals/adc/single_read/adc
+    */
+    // Check if Two Point or Vref are burned into eFuse
+    check_efuse();
+
+    // Configure ADC
+    adc1_config_width(width);
+    adc1_config_channel_atten(channel, atten);
+
+    // Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+    //------------------------------------------
+
     xTaskCreate(task_dht, "task_dht", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
     xTaskCreate(task_teste_queue, "task_teste_queue", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
+    xTaskCreate(task_adc_hl_69, "task_adc_hl_69", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
 }
 //---------------------------------------------------------
+
+
 
 //---------Implementação das tarefas (tasks)----------------
 void task_dht(void *pvParameters)
@@ -82,6 +128,86 @@ void task_teste_queue(void *pvParameters)
             }
         }
         vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+void task_adc_hl_69(void *pvParameters)
+{
+    while (1)
+    {
+        uint32_t adc_reading = 0;
+        // Multisampling
+        for (int i = 0; i < NO_OF_SAMPLES; i++)
+        {
+            if (unit == ADC_UNIT_1)
+            {
+                adc_reading += adc1_get_raw((adc1_channel_t)channel);
+            }
+            else
+            {
+                int raw;
+                adc2_get_raw((adc2_channel_t)channel, width, &raw);
+                adc_reading += raw;
+            }
+        }
+        adc_reading /= NO_OF_SAMPLES;
+        // Convert adc_reading to voltage in mV
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+//--------------------------------------------------------------------
+
+//------------Implementação das funções-------------------------------
+
+/*
+Função disponível no exemplo "adc1_example_main.c" do fabricante Espressif
+que pode ser encontrada no link:
+https://github.com/espressif/esp-idf/tree/master/examples/peripherals/adc/single_read/adc
+*/
+static void check_efuse(void)
+{
+
+    // Check if TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK)
+    {
+        printf("eFuse Two Point: Supported\n");
+    }
+    else
+    {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+    // Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK)
+    {
+        printf("eFuse Vref: Supported\n");
+    }
+    else
+    {
+        printf("eFuse Vref: NOT supported\n");
+    }
+}
+
+/*
+Função disponível no exemplo "adc1_example_main.c" do fabricante Espressif
+que pode ser encontrada no link:
+https://github.com/espressif/esp-idf/tree/master/examples/peripherals/adc/single_read/adc
+*/
+static void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP)
+    {
+        printf("Characterized using Two Point Value\n");
+    }
+    else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF)
+    {
+        printf("Characterized using eFuse Vref\n");
+    }
+    else
+    {
+        printf("Characterized using Default Vref\n");
     }
 }
 
