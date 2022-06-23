90bdcbf98c52d327dd8552766c719ed5d259ea83
guilherme.valese@hotmail.com <guilherme.valese@hotmail.com>
2022-06-22 21:44:33 -0300

    Adicionado firebase

---
+++ Firebase/main/display.hpp
@@ -0,0 +1,175 @@
+/**
+ * @file display.c
+ * @brief Gerencia o display
+ * @version 0.1
+ * @date 2022-05-28
+ */
+#include "freertos/FreeRTOS.h"
+#include <freertos/task.h>
+#include <driver/gpio.h>
+#include <../components/ESP32-RTOS-SSD1306/ssd1306/ssd1306.h>
+#include <driver/i2c.h>
+#include <esp_err.h>
+#include "config.h"
+#include <../components/ESP32-RTOS-FONTS/fonts/fonts.h>
+#include <stdlib.h>
+
+#define DISPLAY_I2C_ADDRESS     0x3C
+#define DISPLAY_WIDTH           128
+#define DISPLAY_HEIGHT          64
+
+#define I2C_MASTER_PORT         I2C_NUM_0
+#define I2C_SDA_PIN             4
+#define I2C_SCL_PIN             5
+
+typedef enum
+{
+    NAO_INICIADO = -1,
+    DISPLAY_OK = 0,
+    ERRO,
+} display_status_t;
+
+display_status_t display_status;
+ssd1306_t display;
+const font_info_t *font = NULL;
+static uint8_t buffer[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
+
+#define I2C_MASTER_SCL_IO 22        /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO 21        /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM 0            /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ 400000   /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS 1000
+
+extern "C" {
+/**
+ * @brief Inicializa o display e configura o I2C
+ */
+void display_init()
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+
+    display_status = NAO_INICIADO;
+
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+    };
+
+    conf.master.clk_speed = 400000;
+
+    esp_err_t err;
+    err = i2c_param_config(I2C_MASTER_PORT, &conf);
+    ESP_ERROR_CHECK(err);
+    err = i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    ESP_ERROR_CHECK(err);
+
+    if (err != ESP_OK)
+        return;
+    
+    ESP_LOGI("I2C", "Iniciado corretamente");
+
+    display.i2c_port = I2C_MASTER_PORT;
+    display.i2c_addr = DISPLAY_I2C_ADDRESS;
+    display.screen = SSD1306_SCREEN;
+    display.width = DISPLAY_WIDTH;
+    display.height = DISPLAY_HEIGHT;
+
+    if (ssd1306_init(&display) != 0)
+    {
+        ESP_LOGE("Display", "Falha na inicializacao");
+        display_status = ERRO;
+        return;
+    }
+
+    ssd1306_set_whole_display_lighting(&display, false);
+    printf("OLED OK!\nTamanho do buffer %d\n", sizeof(buffer));
+
+    font = font_builtin_fonts[FONT_FACE_GLCD5x7];
+
+    display_status = DISPLAY_OK;
+}
+
+/**
+ * @brief Envia uma string para o display
+ *
+ * @param str String
+ * @param x posição X
+ * @param y posição Y
+ */
+void display_write_string(const char *str, uint8_t x, uint8_t y)
+{
+    ssd1306_draw_string(&display, buffer, font, x, y, str, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    ssd1306_load_frame_buffer(&display, buffer);
+}
+
+/**
+ * @brief Escreve um número float no display
+ *
+ * @param num float
+ * @param x posição X
+ * @param y posição Y
+ */
+void display_write_float(float num, uint8_t x, uint8_t y)
+{
+    // TODO:
+    // TESTAR
+
+    char str[13];
+    sprintf(str, "%g", num);
+    display_write_string(str, x, y); // sends the string
+}
+
+void clear_buffer(void)
+{
+    unsigned int i;
+
+    for (i = 0; i < sizeof(buffer); i++)
+    {
+        buffer[i] = 0x00;
+    }
+}
+
+/**
+ * @brief Realiza um teste no display
+ */
+// void display_test(void)
+// {
+//     display_write_string("PI3_2022_1", 0, 8);
+
+//     vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+//     printf("Iniciando animacao\n");
+//     uint8_t i, j;
+
+//     for (i = 0; i < 50; i++)
+//     {
+//         for (j = 9; j > 0; j--)
+//         {
+//             display_load_bitmap(monkeyAnimation[j]);
+//             vTaskDelay(50 / portTICK_PERIOD_MS);
+//         }
+//     }
+
+//     printf("OK\n");
+// }
+
+/**
+ * @brief Exibe um bitmap no display
+ *
+ * @param bitmap
+ */
+void display_load_bitmap(unsigned char *bitmap)
+{
+    clear_buffer();
+
+    if (ssd1306_load_xbm(&display, bitmap, buffer))
+        ESP_LOGE("Display", "Erro ao carregar o bitmap");
+}
+
+}
+
---
+++ Firebase/main/firebase_config.h
@@ -0,0 +1,13 @@
+// Wifi Credentials
+#define SSID "rasp"
+#define PASSWORD "a18g22c02"
+
+// Read readme.md to properly configure api key and authentication
+
+// create a new api key and add it here 
+#define API_KEY "AIzaSyDLVTKNaeNSG_lSELBeVdZoQPbkmXfg-Ow"
+// Copy your firebase real time database link here 
+#define DATABASE_URL "https://pi-3-esp32-default-rtdb.firebaseio.com/"  
+
+#define USER_EMAIL "testemail@gmail.com"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
+#define USER_PASSWORD "testpass"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
