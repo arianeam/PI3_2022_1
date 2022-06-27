e68d19a9110a27cfb6dd4e3bf43dab1802983792
arianeam <47660205+arianeam@users.noreply.github.com>
2022-06-27 11:52:54 -0300

    Update main.cpp

--- Firebase/main/main.cpp
+++ Firebase/main/main.cpp
@@ -18,8 +18,8 @@
 #include "display.h"
 #include "sorriso.h"
 
-//#include <../esp-idf-lib/components/dht/dht.h>
-#include "dht11.h"
+//#include "dht11.h"
+#include "../components/dht/dht.h"
 
 void task_display(void *pvParameters);
 void task_dht(void *pvParameters);
@@ -125,17 +125,27 @@ void task_display(void *pvParameters)
 
 void task_dht(void *pvParameters)
 {
-    dht11 dht11_1;
-    dht11_1.dht11_init();
+    // dht11 dht11_1;
+    // dht11_1.dht11_init();
     float temperatura = -100;
     float umidade = -100;
+    gpio_set_pull_mode(GPIO_NUM_18, GPIO_PULLUP_ONLY);
 
     while (1)
     {
-        dht11_1.read_dht11();
-        temperatura = dht11_1.get_temp();
-        umidade = dht11_1.get_umid();
-        printf("Umidade recebida: %.1f%% Temperatura recebida: %.1fC\n", umidade, temperatura);
+        // dht11_1.read_dht11();
+        // temperatura = dht11_1.get_temp();
+        // umidade = dht11_1.get_umid();
+        // printf("Umidade recebida: %.1f%% Temperatura recebida: %.1fC\n", umidade, temperatura);
+        // vTaskDelay(pdMS_TO_TICKS(2000));
+        if (dht_read_float_data(DHT_TYPE_DHT11, GPIO_NUM_18, &umidade, &temperatura) == ESP_OK)
+        {
+            printf("Umidade: %.1f%% Temp: %.1fC\n", umidade, temperatura);
+        }
+        else
+        {
+            printf("Erro ao ler dados do sensor dht11\n");
+        }
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
